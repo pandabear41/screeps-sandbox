8bfa6e76ff461e6d21c350e573183f9d68f56c1f
Chris Nord <chrispandabear41@gmail.com>
2016-08-03 09:24:55 -0700

    Testing stuff

---
+++ helperMemory.js
@@ -0,0 +1,17 @@
+
+
+module.exports = function () {
+
+};
+
+
+helperMemory.prototype = {
+	initialize: function () {
+
+	},
+	test: function () {
+		console.log("Working");
+	},
+
+	type: 'helperMemory'
+};
